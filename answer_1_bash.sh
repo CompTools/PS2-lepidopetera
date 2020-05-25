7fd428a7d5d0fe255e57cf9e0a8a370e0ad13834
Amanda Markee <56971761+amandamarkee@users.noreply.github.com>
2020-05-25 15:00:13 -0400

    Update answer_1_bash.sh

--- answer_1_bash.sh
+++ answer_1_bash.sh
@@ -6,11 +6,11 @@ grep "GNV" flights.May2017-Apr2018.csv > GNV_flight_data.txt
 egrep "\"[0-9]{4}\",[0-9]*.{3},1" GNV_flight_data.txt > delayed_flights.txt
 
 #Regex expression for departure gainsville delays
-egrep -c "[0-9]{4},\"GNV\",.*\"..\",\"[0-9]{4}\",1.00" delayed_flights.txt
-egrep -c "\",\"GNV\".+[0-9]{4}\",[0-9]+.[0-9]{2},1.00" delayed_flights.txt
-
 #\d{4},"GNV",.*"..","\d{4}",1.00
+egrep -c "[0-9]{4},\"GNV\",.*\"..\",\"[0-9]{4}\",1.00" delayed_flights.txt
+
 #Regex expression for arrival gainsville delays
 #","GNV".+"\d{4}",\d+.\d{2},1.00
+egrep -c "\",\"GNV\".+[0-9]{4}\",[0-9]+.[0-9]{2},1.00" delayed_flights.txt
 
-24 flights that got delayed
+
