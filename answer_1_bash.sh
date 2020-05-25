1d89c3bf10d107347503b10f36395e6c09670172
Amanda Markee <56971761+amandamarkee@users.noreply.github.com>
2020-05-25 13:56:11 -0400

    Update answer_1_bash.sh

--- answer_1_bash.sh
+++ answer_1_bash.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# My first script
 
 grep "GNV" flights.May2017-Apr2018.csv > GNV_flight_data.txt
-echo "Hello World!"
 "\d{4}",\d+.\d{2},([0-1]).00,"\d{4}",\d+.00,1.00
 
 egrep "\"[0-9]{4}\",1.00" GNV_flight_data.txt >delayed_flights.txt
