c7c88bcc38d740fb83f7b0e191f6f71c7262b3ca
Amanda Markee <56971761+amandamarkee@users.noreply.github.com>
2020-05-25 15:44:49 -0400

    Update answer_2_bash.sh

--- answer_2_bash.sh
+++ answer_2_bash.sh
@@ -7,4 +7,10 @@ egrep "\"[0-9]{4}\",[0-9]*.{3},1" GNV_flight_data.txt > delayed_flights.txt
 #Regex equivalent for troubleshooting
 #"\d{4}",\d+.\d{2},([0-1]).00,"\d{4}",\d+.00,1.00
 
+#Counts the total number of flights that depart GNV. This will be used to 
+#manipulate the rest of the table
+egrep -c \[0-9]{4}-[0-9]*-[0-9]*,.[0-9]*,\"GNV\" GNV_flight_data.txt 
+# 2157 total flights departing GNV.
+
+
 
