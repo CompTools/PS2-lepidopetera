2961ce140c676d3251cbdcea7574549be76fd0b1
hendr1lk <57650444+hendr1lk@users.noreply.github.com>
2020-05-25 15:34:31 -0400

    Update answer_4_bash.sh

--- answer_4_bash.sh
+++ answer_4_bash.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# use egrep to select Florida cities, then use sort to get unique cities, and list them in output file called Florida_cities.txt
 egrep -o "[A-Za-z. ]*, FL" flights.May2017-Apr2018.csv | sort --unique > Florida_cities.txt
  
+# needs to be ran on cluster, because flights.May2017-Apr2018.csv is not in repo, file size too large
